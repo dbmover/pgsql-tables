b6c6de6a37353ff30cd23894023c7a183a01c606
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-17 08:35:54 +0200

    only migrate actual changes

--- src/Plugin.php
+++ src/Plugin.php
@@ -24,28 +24,70 @@ class Plugin extends Tables\Plugin
                 column_name,
                 column_default,
                 is_nullable,
-                data_type column_type
+                data_type column_type,
+                udt_name _type,
+                character_maximum_length
             FROM INFORMATION_SCHEMA.COLUMNS
-            WHERE ((TABLE_CATALOG = ? AND TABLE_SCHEMA = 'public') OR TABLE_SCHEMA = ?)
-                AND TABLE_NAME = ?
+            WHERE TABLE_CATALOG = ? AND TABLE_SCHEMA = 'public' AND TABLE_NAME = ?
             ORDER BY ORDINAL_POSITION ASC");
     }
 
-    protected function modifyColumn(string $table, string $column, array $definition) : string
+    protected function modifyColumn(string $table, string $column, array $definition, array $current) : array
     {
-        $sql = '';
-        $sql .= "ALTER TABLE $table ALTER COLUMN $column TYPE {$definition['column_type']};";
-        if (strlen($definition['column_default'])) {
-            $sql .= "ALTER TABLE $table ALTER COLUMN $column SET DEFAULT {$definition['_default']};";
-        } else {
-            $sql .= "ALTER TABLE $table ALTER COLUMN $column DROP DEFAULT;";
+        if ($current['column_type'] == 'CHARACTER VARYING') {
+            $current['column_type'] = "VARCHAR({$current['character_maximum_length']})";
+        } elseif ($current['column_type'] == 'CHARACTER') {
+            $current['column_type'] = "CHAR({$current['character_maximum_length']})";
+        } elseif ($current['column_type'] == 'DOUBLE PRECISION') {
+            $current['column_type'] = 'FLOAT';
+        } elseif ($current['column_type'] == 'ARRAY') {
+            switch ($current['_type']) {
+                case '_int2': $current['column_type'] = 'SMALLINT[]'; break;
+                case '_int4': $current['column_type'] = 'MEDIUMINT[]'; break;
+                case '_int8': $current['column_type'] = 'BIGINT[]'; break;
+                case '_varchar': $current['column_type'] = 'VARCHAR[]'; break;
+                case '_float8': $current['column_type'] = 'FLOAT[]'; break;
+            }
         }
-        if ($definition['is_nullable']) {
-            $sql .= "ALTER TABLE $table ALTER COLUMN $column DROP NOT NULL;";
-        } else {
-            $sql .= "ALTER TABLE $table ALTER COLUMN $column SET NOT NULL;";
+        $sql = [];
+        if ($definition['column_type'] != $current['column_type']) {
+            var_dump($definition['column_type'], $current['column_type']);
+            $sql[] = "ALTER TABLE $table ALTER COLUMN $column TYPE {$definition['column_type']};";
         }
+        if (!is_null($current['column_default'])) {
+            // Default strings are stored internally as 'string'::character varying
+            $current['column_default'] = preg_replace("@^'(\w+)'::.*?$@", '\\1', $current['column_default']);
+        }
+        if (!is_null($definition['column_default'])) {
+            if ($this->defaultsDiffer($definition['column_default'], $current['column_default'] ?? '')) {
+                $sql[] = "ALTER TABLE $table ALTER COLUMN $column SET DEFAULT {$definition['_default']};";
+            }
+        } else {
+            if ($current['column_default'] != null) {
+                $sql[] = "ALTER TABLE $table ALTER COLUMN $column DROP DEFAULT;";
+            }
+        }
+        if ($definition['is_nullable'] && !$current['is_nullable']) {
+            $sql[] = "ALTER TABLE $table ALTER COLUMN $column DROP NOT NULL;";
+        } elseif (!$definition['is_nullable'] && $current['is_nullable']) {
+            $sql[] = "ALTER TABLE $table ALTER COLUMN $column SET NOT NULL;";
+        }
+        if ($sql) var_dump($table, $sql);
         return $sql;
+    }
+
+    protected function defaultsDiffer(string $definition, string $current) : bool
+    {
+        if (strpos($current, '::timestamp')) {
+            // This doesn't worry about time zones right now -
+            // if anyone feels like writing a sane implementation, be my guest
+            // (but I've yet to see a schema where anything else than "the
+            // current timestamp" is used).
+            preg_match("@'(.*?)\+\d{2}'::@", $current, $extr);
+            $current = $extr[1];
+            return strtotime($current) != strtotime($definition);
+        }
+        return strtolower($current) != trim(strtolower($definition));
     }
 }
 
